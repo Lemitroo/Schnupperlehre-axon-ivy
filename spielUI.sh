e83cb10efc25fdc6fecddad4492704cf302f7f71
Lemitroo <la.schmitter@gmail.com>
2021-03-16 16:27:15 +0100

    added calculator

--- spielUI.sh
+++ spielUI.sh
@@ -6,7 +6,7 @@ startgame () {
     guessednumber=0
     Tries=0
   while [ "$guessednumber" -ne "$generatetnumber" ]; do 
-    guessednumber=$(dialog --inputbox "Guess the number: $generatetnumber" 10 30 2>&1 >/dev/tty)
+    guessednumber=$(dialog --inputbox "Guess the number:" 10 30 2>&1 >/dev/tty)
     if [ "$generatetnumber" -gt "$guessednumber" ]; then 
         dialog --msgbox "The guessed number is not corect, the number has to be higher!" 10 30
     elif [ "$generatetnumber" -lt "$guessednumber" ]; then
